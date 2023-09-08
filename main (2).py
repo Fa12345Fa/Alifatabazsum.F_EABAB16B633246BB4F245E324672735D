568c1408e80eaca462fe3be6fadc25587d07b9eb
Fa12345Fa <144419580+Fa12345Fa@users.noreply.github.com>
2023-09-08 11:48:08 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,13 @@
+# write python program to check whether the given number is leap or not.
+def isLeapYear(year):
+  if (year% 4==0 and year % 100 !=0) or year %400==0:
+      return True
+  else:
+     return False
+
+year =int(input("Enter a year : "))
+
+if isLeapYear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year))
